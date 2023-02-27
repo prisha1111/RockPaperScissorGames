d5325b4fa02b77bcb0480d670ed59a5f10ee8ddb
Revabindra <122342037+Revabindra@users.noreply.github.com>
2023-02-27 19:58:17 +0530

    Add files via upload

---
+++ vowel2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	char c;
+	printf("Enter a vowel: ");
+	scanf("%c",&c);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+		printf("The character %c is a vowel.\n",c);
+	}
+	else{
+		printf("The character %c is a consonant.\n",c);
+	}
+	return 0;
+}
